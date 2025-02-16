59bba979e42a873b5dec8d83bcd9b231708b4318
kreker92 <zvv1992@gmail.com>
2025-02-16 23:59:41 +0300

    FIX cypress order

--- cypress/e2e/constructor/constructor.cy.ts
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -1,3 +1,5 @@
+import orderNew from '../../fixtures/order-new.json';
+
 describe('Constructor', () => {
   beforeEach(() => {
     cy.intercept('POST', 'api/auth/user', {
@@ -12,6 +14,9 @@ describe('Constructor', () => {
     cy.intercept('GET', 'api/orders', {
       fixture: 'orders'
     });
+    cy.intercept('POST', 'api/orders', {
+      fixture: 'order-new'
+    });
     cy.visit('');
     // window.localStorage.setItem(
     //   'refreshToken',
@@ -20,11 +25,21 @@ describe('Constructor', () => {
     // cy.setCookie('accessToken', 'test-accessToken');
   });
 
-  it('should show order button on ingredient click', () => {
+  it('should show order modal with correct number', () => {
+    cy.get('a[href="/profile"]').click();
+    const email = '2@qwe.ru';
+    const password = '123456';
+    cy.get('[data-testid=email_input').type(`${email}{enter}`);
+    cy.get('[data-testid=password_input').type(`${password}{enter}`);
+    cy.get('a[href="/"]').first().click();
+
     cy.get('li[data-testid=643d69a5c3f7b9001cfa093c] > button').click();
     const orderButton = cy.get('button[data-testid="order-button"]');
-    orderButton.contains('Оформить заказ');
-    // orderButton.click();
+    // orderButton.contains('Оформить заказ');
+    orderButton.click();
+    cy.get(`h2[data-testid="${orderNew.order.number}"]`).contains(
+      orderNew.order.number
+    );
   });
 
   it('should show ingredient modal', () => {
